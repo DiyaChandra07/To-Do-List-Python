0d808f725b5f3422b80cb432cb0f0f1e24b4918f
DiyaChandra07 <diyachandra807@gmail.com>
2026-06-26 20:08:45 +0400

    Changes to README

--- ToDo_List_Python_Project.py
+++ ToDo_List_Python_Project.py
@@ -1,104 +1,212 @@
-import tkinter as tk
 
-root=tk.Tk()
+import tkinter as tk
+from datetime import datetime
+
+# COLOURS
+High_green="#364025"
+Low_green="#cfbb9a"
+Mid_green="#899064"
+Beige_bg="#E5D7C4"
+Header_brown="#4b3d1a"
+High_green_text="#b7c77e"
+Low_green_text="#4b3d1a"
+Mid_green_text="#d3d488"
+
+
+#ROOT
+root = tk.Tk()
 root.title("✦ Get It Done ✦")
-root.state("zoomed")    #used "zoomed" to open in full screen
-root.config()
+root.state("zoomed")  #keep the screen filled
+root.config(bg=Beige_bg) #setting bg colour
+
 
 
 # HEADER FRAME
-header = tk.Frame(root, bg="#4b3d1a", height=80)
+header = tk.Frame(root, bg=Header_brown, height=80)
 header.pack(side="top", fill="x")
-# Prevent shrinking
 header.pack_propagate(False)
 
-#Creating frames in header frame
-left=tk.Frame(header, bg="#4b3d1a")
-left.pack(side="left",padx=20)
+left = tk.Frame(header, bg=Header_brown)
+left.pack(side="left", padx=20)
 
-center=tk.Frame(header, bg="#4b3d1a")
-center.pack(side="left",expand=True)    #Tkinter does not take "center" so create a left widget that expands into the avail space
+center = tk.Frame(header, bg=Header_brown)
+center.pack(side="left", expand=True)
 
-right=tk.Frame(header, bg="#4b3d1a")
-right.pack(side="right",padx=20)
+right = tk.Frame(header, bg=Header_brown)
+right.pack(side="right", padx=20)
 
-#date for left frame
-def left_text():
-    
-    from datetime import datetime
-    
-    now = datetime.now()
-    formatted = now.strftime("%B %d • %A").lower()
-    return formatted
-    
-left_label=tk.Label(left,text=left_text(), font = ("Times New Roman",15),bg="#4b3d1a",fg="#cfbb9a")
-left_label.pack(side="left")
+# date in header frame
+def get_date():
+    return datetime.now().strftime("%B %d • %A").lower()  # to get the present day date
 
-#center frame title
-center_label=tk.Label(center,text="✦ Get it Done ✦",font=("Times New Roman",25),bg="#4b3d1a",fg="#cfbb9a") #label under center frame
-center_label.pack()
+tk.Label(left, text=get_date(), bg=Header_brown, fg=Low_green, font=("Times New Roman", 16)).pack()
 
-#right motivational text + menu option
-right_label=tk.Label(right,text="☘︎ one step at a time       ☰",font=("Times New Roman",15),bg="#4b3d1a",fg="#cfbb9a") #
-right_label.pack()
+tk.Label(center, text="✦ Get It Done ✦", bg=Header_brown, fg=Low_green, font=("Times New Roman", 26, "bold")).pack()
 
 
 
-#MAIN FRAME
-main = tk.Frame(root, bg="#e6d7c4")
-main.pack(fill="both",expand=True)  #creating main frame under root filling x and y axis
+# MAIN FRAME
+main = tk.Frame(root, bg=Beige_bg)
+main.pack(fill="both", expand=True)  #creating frame for whole main screen
 
-# Make grid cells expand
-canvas = tk.Canvas(main, bg="#E5D7C4", highlightthickness=0)
-scrollbar = tk.Scrollbar(main, orient="vertical", command=canvas.yview)
+canvas = tk.Canvas(main, bg=Beige_bg, highlightthickness=0)
+canvas.pack(side="left", fill="both", expand=True)
 
-scrollable_frame = tk.Frame(canvas, bg="#E5D7C4")
-
-scrollable_frame.bind(
-    "<Configure>",
-    lambda e: canvas.configure(scrollregion=canvas.bbox("all"))
-)
-
-canvas.create_window((20, 20), window=scrollable_frame, anchor="nw", width=canvas.winfo_screenwidth())
-
+# Scrollbar
+scrollbar = tk.Scrollbar(main, command=canvas.yview)
+scrollbar.pack(side="right", fill="y")    #for ease of accessibility (scrollable screen)
 canvas.configure(yscrollcommand=scrollbar.set)
 
-canvas.pack(side="left", fill="both", expand=True)
-scrollbar.pack(side="right", fill="y")
+frame = tk.Frame(canvas, bg=Beige_bg)
+canvas.create_window((0, 0), window=frame, anchor="nw", width=canvas.winfo_width())
+def resize_frame(event):
+    canvas.itemconfig("all", width=event.width)
+
+canvas.bind("<Configure>", resize_frame)
+frame.bind("<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all")))  # to make sure the content  stretches and fills the frame
+
+for i in range(4):
+    frame.columnconfigure(i, weight=0)
+
+# FILE-HANDLING 
+FILE = "tasks.txt"
+tasks = []   # store title - notes - priority
+
+#priority management
+def get_color(priority):
+    if priority == "high":
+        return High_green
+    elif priority == "medium":
+        return Mid_green
+    else:
+        return Low_green
+
+def get_text_color(priority):
+    if priority == "high":
+        return High_green_text
+    elif priority == "medium":
+        return Mid_green_text
+    else:
+        return Low_green_text
 
 
-# -------- GRID CONFIG (IMPORTANT) --------
-for j in range(4):
-    scrollable_frame.columnconfigure(j, weight=1)
+def save():
+    with open(FILE, "w") as f:
+        for t in tasks:
+            f.write(f"{t[0]}|{t[1]}|{t[2]}\n")
+
+def load():
+    try:
+        with open(FILE) as f:
+            for line in f:
+                parts = line.strip().split("|")  # remove space and save separate parts
+                if len(parts) == 3:
+                    tasks.append(parts)
+    except:
+        pass
+
+# CARDS
+def show_tasks():
+    for w in frame.winfo_children():
+        w.destroy()      # to destroy cards after every time window opens
+
+    for i, t in enumerate(tasks):
+        r = i // 4
+        c = i % 4
+        bg = get_color(t[2])
+        fg = get_text_color(t[2])
+
+        # outer frame for each card in the grid
+        outer = tk.Frame(frame, bg=Beige_bg, width=340, height=260)
+        outer.grid(row=r, column=c, padx=35, pady=20)
+        outer.grid_propagate(False)
+
+        # card placed inside the outer frame
+        card = tk.Frame(outer, bg=bg, width=290, height=200)
+        card.pack(padx=8, pady=8)
+        card.pack_propagate(False)
+
+        tk.Label(card, text=t[0], bg=bg, fg=fg, font=("Times New Roman", 18, "bold",)).pack(pady=(10, 5))
+
+        tk.Label(card, text=t[1], bg=bg, fg=fg, font=("Times New Roman", 15), wraplength=230, justify="left").pack(pady=5)
+        # each delete button for each card i for each card
+        tk.Button(card, text="Delete", bg=Beige_bg, fg=Header_brown, activebackground="#7a2a2a", command=lambda i=i: delete(i)).pack(side="bottom", pady=8)
+        
+
+def delete(i):
+    tasks.pop(i)
+    save()
+    show_tasks()
+
+# ADD TASK
+def open_popup():
+    pop = tk.Toplevel(root)
+    pop.title("New Task")
+    pop.geometry("320x320+600+200")  #aligning the new window
+    pop.config(bg=Beige_bg)
+
+    tk.Label(pop, text="✦ New Task ✦", bg=Beige_bg, font=("Times New Roman", 20, "bold"),fg=Header_brown).pack(pady=10)
+
+    tk.Label(pop, text="Title", bg=Beige_bg,fg=Header_brown).pack()
+    title = tk.Entry(pop, font=("Times New Roman", 13))
+    title.pack(pady=5)
+    
+
+    tk.Label(pop, text="Notes", bg=Beige_bg, fg=Header_brown).pack()
+    notes = tk.Entry(pop, font=("Times New Roman", 13))
+    notes.pack(pady=5)
+
+    pr = tk.StringVar(value="low") 
+
+    tk.Label(pop, text="Priority", bg=Beige_bg, fg=Header_brown).pack(pady=5)
+    
+    # choice for priority
+    tk.Radiobutton(pop, text="High", variable=pr, value="high", bg=Beige_bg,fg=Header_brown).pack()
+    tk.Radiobutton(pop, text="Medium", variable=pr, value="medium", bg=Beige_bg,fg=Header_brown).pack()
+    tk.Radiobutton(pop, text="Low", variable=pr, value="low", bg=Beige_bg,fg=Header_brown).pack()
+
+    def add():
+        if title.get() != "":
+            tasks.append([title.get(), notes.get(), pr.get()])
+            save()
+            show_tasks()
+            pop.destroy()
+
+    tk.Button(pop, text="Add", bg=Header_brown, fg=Low_green, activebackground="#80714c", font=("Times New Roman", 11), command=add).pack(pady=10)
+
+# SORT OPTION
+def sort_tasks():
+    order = {"high": 0, "medium": 1, "low": 2}
+    tasks.sort(key=lambda x: order[x[2]])
+    save()
+    show_tasks()
+
+#CLEAT OPTION
+def clear():
+    tasks.clear()
+    save()
+    show_tasks()
+
+# MENU
+menu_btn = tk.Menubutton(right, text="☰", bg=Header_brown, fg=Low_green, font=("Times New Roman", 18))
+menu = tk.Menu(menu_btn, tearoff=0)
+
+menu.add_command(label="Sort by Priority", command=sort_tasks)
+menu.add_command(label="Clear All", command=clear)
+
+menu_btn.config(menu=menu)
+menu_btn.pack()
+
+# ADD BUTTON
+tk.Button(root, text="+", font=("Times New Roman", 26, "bold"), bg=Low_green, fg=Header_brown,
+          activebackground="#80714c", width=3, height=1,
+          command=open_popup).place(relx=0.95, rely=0.9, anchor="center")
 
 
-# -------- CARD FUNCTION --------
-def create_card(parent, row, column, title, notes, bg_colour, fg_colour):
 
-    parent.rowconfigure(row, weight=1)
+# MAIN CALLING OF FUNCTION
+load()
+show_tasks()
 
-    card = tk.Frame(parent, bg=bg_colour, width=200, height=180)
-    card.grid(row=row, column=column, padx=20, pady=20, sticky="nsew")
-    card.grid_propagate(False)
-
-    title_label = tk.Label(card, text=title, font=("Times New Roman", 17),
-                           bg=bg_colour, fg=fg_colour)
-    title_label.pack(pady=(10, 5))
-
-    notes_label = tk.Label(card, text=notes, font=("Times New Roman", 14),
-                           bg=bg_colour, fg=fg_colour,
-                           anchor="nw", justify="left", wraplength=220)
-    notes_label.pack(padx=10, pady=(0, 10), fill="both", expand=True)
-for i in range(20):
-    row = i // 4
-    col = i % 4
-    create_card(
-        scrollable_frame,
-        row,
-        col,
-        f"✦ TASK {i+1}",
-        "Sample task text kuhefiuwhefi uwihgiwhgi wiueghwugh iuwehfouweh ihef iwueghi wiegh iwuegh ig iwueg eighwieguh eg woegy eg",
-        "#899064",
-        "#E5D7C4"
-    )
 root.mainloop()
+
